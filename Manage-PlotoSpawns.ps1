d57095b8ad77c67116bf7add1276c8c5fa38e5fe
tydeno <83050419+tydeno@users.noreply.github.com>
2021-04-24 11:55:13 +0200

    Update Manage-PlotoSpawns.ps1

--- Manage-PlotoSpawns.ps1
+++ Manage-PlotoSpawns.ps1
@@ -5,16 +5,29 @@ function Manage-PlotoSpawns
 
     Do
     {
-        Write-Host "Initiating Spawning..."
+        Write-Host "PlotoManager @"(Get-Date)": Initiating Spawning..."
  
         $SpawnedPlots = Spawn-PlotoPlots
-        $SpawnedCount + $SpawnedPlots.Count | Out-Null
-        Write-Host "Amount of spawned Plots in this iteration: " $SpawnedPlots.Count
-        Write-Host "Overall spawned Plots since start of script: "$SpawnedCount
-        
 
-        Write-Host "Entering Sleep for 3600, then checking again for available temp and out drives"
-        Start-Sleep 3600
+        if ($SpawnedPlots)
+            {
+                Write-Host "PlotoManager @"(Get-Date)": Amount of spawned Plots in this iteration: " $SpawnedPlots.Count
+                Write-Host "PlotoManager @"(Get-Date)": Spawned the following plots using Ploto Spawner:" 
+                Write-Host $SpawnedPlots | ft
+                
+            }
+        else
+            {
+                Write-Host "PlotoManager @"(Get-Date)": No plots spawned in thy cycle, as no temp disks available"
+            }
+
+
+        $SpawnedCount = $SpawnedCount + $SpawnedPlots.Count 
+       
+        Write-Host "PlotoManager @"(Get-Date)": Overall spawned Plots since start of script: "$SpawnedCount
+        Write-Host "PlotoManager @"(Get-Date)": Entering Sleep for 900, then checking again for available temp and out drives"
+        Write-Host "----------------------------------------------------------------------------------------------------------------------"
+        Start-Sleep 900
     }
     
     Until ($SpawnedCount -eq $InputAmountToSpawn)
