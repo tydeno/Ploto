f3d4b6245e6243291a317d1e4370b82e8b8a1dc3
tydeno <83050419+tydeno@users.noreply.github.com>
2021-04-24 19:07:24 +0200

    Update Move-PlotoFinalPlots.ps1

--- Move-PlotoFinalPlots.ps1
+++ Move-PlotoFinalPlots.ps1
@@ -1,41 +1,4 @@
-function Get-PlotoOutDrives
-{
-
-$tmpDrives = get-WmiObject win32_logicaldisk | ? {$_.VolumeName -like "*plot*"}
-$outDrives = get-WmiObject win32_logicaldisk | ? {$_.VolumeName -like "*out*"}
-
-#Check Space for outDrives
-$collectionWithDisks= New-Object System.Collections.ArrayList
-foreach ($drive in $outDrives)
-    {
-  
-        $FreeSpace = [math]::Round($drive.FreeSpace  / 1073741824, 2)
-        If ($FreeSpace -gt 107)
-            {
-                $PlotToDest = $true
-            }
-        else
-            {
-                $PlotToDest = $false
-            }
-
-        $outdriveToPass = [PSCustomObject]@{
-        DriveLetter     =  $drive.DeviceID
-        ChiaDriveType = "Out"
-        VolumeName = $drive.VolumeName
-        FreeSpace = $FreeSpace
-        IsPlottable    = $PlotToDest
-        AmountOfPlotsToHold = [math]::Floor(($FreeSpace / 100))
-        }
-
-        $collectionWithDisks.Add($outdriveToPass) | Out-Null
-
-    }
-    return $collectionWithDisks 
-}
-
-
-function Get-FinalPlotFile
+function Get-PlotoFinalPlotFile
 {
 
 #Scan for final Plot Files to Move
