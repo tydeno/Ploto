8372b2a8abcbcdf77d4720accbe0aa6c7d10480d
tydeno <83050419+tydeno@users.noreply.github.com>
2021-04-24 18:43:10 +0200

    Update Spawn-PlotoPlots.ps1

--- Spawn-PlotoPlots.ps1
+++ Spawn-PlotoPlots.ps1
@@ -12,44 +12,36 @@ $PlottableOutDrives = Get-PlotoOutDrives -OutDriveDenom $OutDriveDenom | ? {$_.I
 
 $collectionWithPlotJobs= New-Object System.Collections.ArrayList
 
-Write-Host "PlotoSpawner @"(Get-Date)": Checking for available temp and out drives..."
+Write-Host "PlotoSpawner @"(Get-Date)": Checking for available temp and out drives..." 
 
 if ($PlottableTempDrives)
     {
          foreach ($PlottableTempDrive in $PlottableTempDrives)
             {
-                Write-Host "PlotoSpawner @"(Get-Date)": Found available temp drive:"
-                Write-Host $PlottableTempDrive | ft
+                Write-Host "PlotoSpawner @"(Get-Date)": Found available temp drive: "$PlottableTempDrive -ForegroundColor Green
 
                 #Choose most suitable OutDrive (assumed the one with most space)
                 $max = ($PlottableOutDrives | measure-object -Property FreeSpace -maximum).maximum
                 $OutDrive = $PlottableOutDrives | ? { $_.FreeSpace -eq $max}
                 $OutDriveLetter = $OutDrive.DriveLetter
 
-                Write-Host "PlotoSpawner @"(Get-Date)": Found most suitable Out Drive:"
-                Write-Host $OutDrive | ft
-        
-                #stitch together ArgumentsList for chia.exe
-                $ArgumentList = "plots create -k 32 -t "+$PlottableTempDrive.DriveLetter+"\ -d "+$OutDriveLetter+"\ -e"
-
-                Write-Host "PlotoSpawner @"(Get-Date)": Using the following Arguments for Chia.exe:"
-                Write-Host $ArgumentList | ft
-
-                $PathToChia = "$env:LOCALAPPDATA\chia-blockchain\app-1.1.1\resources\app.asar.unpacked\daemon\"
-
-                Write-Host "PlotoSpawner @"(Get-Date)": Using the following Path to chia.exe: "$PathToChia
-                Write-Host "PlotoSpawner @"(Get-Date)": Starting plotting using chia.exe."
-
+                Write-Host "PlotoSpawner @"(Get-Date)": Found most suitable Out Drive: "$OutDrive -ForegroundColor Green
             
                 #Fire off chia
                 try 
                     {
+                        $PathToChia = "$env:LOCALAPPDATA\chia-blockchain\app-1.1.1\resources\app.asar.unpacked\daemon\"                         
+                        $ArgumentList = "plots create -k 32 -t "+$PlottableTempDrive.DriveLetter+"\ -d "+$OutDriveLetter+"\ -e"
+
+                        Write-Host "PlotoSpawner @"(Get-Date)": Using the following Arguments for Chia.exe: "$ArgumentList 
+                        Write-Host "PlotoSpawner @"(Get-Date)": Starting plotting using the following Path to chia.exe: "$PathToChia
+
                         cd $PathToChia
                         Start-Process .\chia.exe -ArgumentList $ArgumentList
                     }
                 catch
                     {
-                        Write-Host "PlotoSpawner @"(Get-Date)": ERROR! Could not launch chia.exe. Check chiapath and arguments (make sure version is set correctly!)"
+                        Write-Host "PlotoSpawner @"(Get-Date)": ERROR! Could not launch chia.exe. Check chiapath and arguments (make sure version is set correctly!). Arguments used: "$ArgumentList -ForegroundColor Red
                     }
 
                 #Deduct 106GB from OutDrive Capacity in Var
@@ -63,11 +55,9 @@ if ($PlottableTempDrives)
                 StartTime = (Get-Date)
                 }
 
-                Write-Host "PlotoSpawner @"(Get-Date)": The following Job was initiated:"
-                Write-Host $PlotJob | ft
-
                 $collectionWithPlotJobs.Add($PlotJob) | Out-Null
 
+                Write-Host "PlotoSpawner @"(Get-Date)": The following Job was initiated: "$PlotJob -ForegroundColor Green
                 Write-Host "--------------------------------------------------------------------"
                 Start-Sleep 900
 
@@ -76,7 +66,7 @@ if ($PlottableTempDrives)
     }
 else
     {
-        Write-Host "PlotoSpawner @"(Get-Date)": No available Temp and or Out Disks found."
+        Write-Host "PlotoSpawner @"(Get-Date)": No available Temp and or Out Disks found." -ForegroundColor Yellow
     }
 
    return $collectionWithPlotJobs
